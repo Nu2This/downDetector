bb74afaacb3241e7f51a930e1f56e221572d5d61
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2018-07-27 16:53:14 -0500

    Saving to work at home

--- down.py
+++ down.py
@@ -2,6 +2,7 @@
 import os
 import time
 import subprocess
+import pudb
 #comment to be removed
 date = time.strftime('%m%y')
 tick = time.strftime('%d @ %T ')
@@ -18,34 +19,30 @@ class colors:
     OK = '\033[36m'
 
 
-def poll(host):
-    """Ping the hosts in the host file"""
-    # Create subprocess object for ping
-    up = True
-    p = subprocess.run('ping -c 1 -W 1 '
-                       + str(host),
-                       stdout=subprocess.PIPE,
-                       shell=True)
-    # Create object to store the text from the ping
-    text = p.stdout.decode('utf-8')
+def parse_text(text):
+    up = -1
     # Split ping response into separate lines for parsing
     line = text.split('\n')
     # Parses the lines from stdout to see if a ping failed or succeeded
     for item in line:
         if '0 packets received' in item:
-            print(str(time.time()) + ' ' + str(host) + ' Failed Ping',
-                  file=open(working_dir + time.strftime('%m%d@' + host)
-                            + '.txt', 'a+'))
-            up = False
+            up = -1
         if 'time' in item:
             ms = item.split('time=', 1)[1]
-            if float(ms.rstrip(' ms')) > 100:
-                print(str(time.time()) + ' ' + str(host) + ' High(' + ms + ')',
-                      file=open(working_dir + time.strftime('%m%d@'+host)
-                                + '.txt', 'a+'))
+            up = ms.strip('ms')
+    return float(up)
 
-    # Ping the host and return True if it is up
-    return up
+
+def poll(entry):
+    """Ping the hosts in the host file"""
+    # Create subprocess object for ping
+    p = subprocess.run('ping -c 1 -W 1 '
+                       + entry['host'],
+                       stdout=subprocess.PIPE,
+                       shell=True)
+        # Create object to store the text from the ping
+    text = p.stdout.decode('utf-8')
+    entry['pms'] = parse_text(text)
 
 
 def init(entry):
@@ -56,59 +53,74 @@ def init(entry):
             data = line.split()
             master.append({'host': data[0], 'label': data[1], 'count': 0, 'pms':0})
 
+def writer(entry):
+    # If entry is up, set counter to zero
+    if entry['pms'] != -1:
+        entry['count'] = 0
+    # If the ping is over the limit print to file
+    if entry['pms'] > 100:
+        print(str(time.time()) + ' ' + entry['host'] + ' High(' + str(entry['pms']) + ')',
+              file=open(working_dir + time.strftime('%m%d@'+ entry['host'])
+                        + '.txt', 'a+'))
+    # If the faild count is over the threshold, print to file and speak
+    if entry['count'] / 2 > 5:
+        upAlert = os.system('espeak ' + entry['label'] + '"is up"')
+        print(tick + entry['host'].ljust(15) + ' Came Up',
+              file=open(CURRENT_DIR + date
+                        + '.txt', 'a'))
+        upAlert
+    # If the ping fails add to the counter and print to file
+    else:
+        entry['count'] = entry['count'] + 1
+        print(str(time.time()) + ' ' + entry['host'] + ' Failed Ping',
+              file=open(working_dir + time.strftime('%m%d@' + entry['host'])
+                        + '.txt', 'a+'))
 
-def ping_hosts():
-    """Use poll function to ping the hosts and change values inside the host
-       dictionary"""
-    for entry in master:
-        # If host is up set counter to zero
-        if poll(entry['host']):
-            # Write to file if host came back from down state and speak
-            if entry['count'] / 2 > 5:
-                upAlert = os.system('espeak ' + entry['label'] + '"is up"')
-                print(tick + entry['host'].ljust(15) + ' Came Up',
-                      file=open(CURRENT_DIR + date
-                                + '.txt', 'a'))
-                upAlert
-            entry['count'] = 0
-        # If host is down add to counter
-        else:
-            entry['count'] = entry['count'] + 1
 
 
-def check_hosts(threshold=5):
+def to_screen(entry, threshold=5):
     """Checks to see if the host is above the threshold and prints to the
        screen"""
     # Write to file the time host goes down
-    os.system('clear')
-    for entry in master:
-        # If it hits the threshold write to file and speak
-        if entry['count'] / 2 == 5:
-            downAlert = os.system('espeak ' + entry['label'] + '"is down"')
-            print(tick + entry['host'].ljust(15) + ' Went Down',
-                  file=open(CURRENT_DIR + date + '.txt', 'a'))
-            downAlert
-            print(entry['host'].ljust(15)
-                  + entry['label'].ljust(13)
-                  + ' is '
-                  + colors.WARN
-                  + 'Down!'
-                  + colors.END)
-        # Print to screen the Status of hosts
-        elif (entry['count'] / 2) > 5:
-            print(entry['host'].ljust(15)
-                  + entry['label'].ljust(13)
-                  + ' is '
-                  + colors.WARN
-                  + 'DOWN!'
-                  + colors.END)
-        else:
-            print(entry['host'].ljust(15)
-                  + entry['label'].ljust(13)
-                  + ' is '
-                  + colors.OK
-                  + 'UP'
-                  + colors.END)
+    # If it hits the threshold write to file and speak
+    if entry['count'] / 2 == 5:
+        downAlert = os.system('espeak ' + entry['label'] + '"is down"')
+        print(tick + entry['host'].ljust(15) + ' Went Down',
+              file=open(CURRENT_DIR + date + '.txt', 'a'))
+        downAlert
+        print(entry['host'].ljust(15)
+              + entry['label'].ljust(13)
+              + ' is '
+              + colors.WARN
+              + 'Down!'
+              + colors.END)
+    # Print to screen the Status of hosts
+    elif (entry['count'] / 2) > 5:
+        print(entry['host'].ljust(15)
+              + entry['label'].ljust(13)
+              + ' is '
+              + colors.WARN
+              + 'DOWN!'
+              + colors.END)
+    elif entry['pms'] > 100:
+        print(entry['host'].ljust(15)
+              + entry['label'].ljust(13)
+              + ' is '
+              + colors.OK
+              + 'UP '
+              + colors.END
+              + colors.WARN
+              + str(entry['pms'])
+              + colors.END)
+
+    else:
+        print(entry['host'].ljust(15)
+              + entry['label'].ljust(13)
+              + ' is '
+              + colors.OK
+              + 'UP '
+              + colors.END
+              + str(entry['pms']))
 
 
 if __name__ == '__main__':
@@ -119,6 +131,9 @@ if __name__ == '__main__':
     print("*****Starting Program*****\n*****" + start + '****',
           file=open(date + '.txt', 'a'))
     while True:
-        ping_hosts()
-        check_hosts()
         time.sleep(1)
+        os.system('clear')
+        for entry in master:
+            poll(entry)
+            writer(entry)
+            to_screen(entry)
